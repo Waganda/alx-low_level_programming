51c2d38ce90584a403f9682f8e53c6ca7a5de680
Marie Otieno <otienomary17@gmail.com>
2022-07-15 02:59:33 -0700

    3. I do not fear computers 2.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,12 @@
  * _puts - prints a string to stdout followed by a new line
  * @str: the string to print
  * Return: the string
- */ 
+ */
 void _puts(char *str)
 {
 	int i;
 
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 
 	_putchar(str[i]);
 	_putchar('\n');
