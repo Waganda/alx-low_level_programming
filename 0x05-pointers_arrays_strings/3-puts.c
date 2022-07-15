920e63f3dffe7e13917bf0043e694442397d721b
Marie Otieno <otienomary17@gmail.com>
2022-07-15 02:57:30 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout followed by a new line
+ * @str: the string to print
+ * Return: the string
+ */ 
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0; str[i] != '\0'; i++)
+
+	_putchar(str[i]);
+	_putchar('\n');
+}
